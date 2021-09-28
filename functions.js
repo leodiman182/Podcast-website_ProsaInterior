07d0a2254ab2eb04d559d5c74472c39887755fcd
Leonardo Diman Martins <leonardo.diman@gmail.com>
2021-09-28 16:55:51 -0300

    NEW PROSA INTERIOR WEBSITE V3

--- functions.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function () {
-    $('div.menu a').click(function(){
-       $('div.menu li').removeClass('active');
-       $(this).parent().addClass('active');
-    })
- });
-
-
- 
--- scripts.js
+++ scripts.js
@@ -1,38 +1,118 @@
-const navSlide = () => {
-    const burger = document.querySelector('.burger');
-    const nav = document.querySelector('.nav-links');
-    const navLinks = document.querySelectorAll('.nav-links li')
-    
-    
-    
-    burger.addEventListener('click',()=>{
-        //Toggle nav
-        nav.classList.toggle('nav-active');
+/* TOGGLE MENU SCRIPT */
 
-        //Animate links
-        navLinks.forEach((link, index) => {
-            if (link.style.animation) {
-                link.style.animation = '';
-            } else {
-                link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 0.2}s`;
-            }        
-        });
-        // Burger animation
-        burger.classList.toggle('toggle')
+const nav = document.querySelector('#header nav')
+const toggle = document.querySelectorAll('nav .toggle')
 
-    });
-  
+for (const element of toggle) {
+  element.addEventListener('click', function () {
+    nav.classList.toggle('show')
+  })
 }
-navSlide();
 
-/*
+/* ESCONDER MENU COM LINKS */
 
-const changePic = () => {
-    const pic1 = document.querySelector('pic1');
-    const pic2 = document.querySelector('pic2');
-    const pics = document.querySelector('pics');
-    
-    nav.classList.toggle('pics');
-    
+const links = document.querySelectorAll('nav ul li a')
+
+for (const link of links) {
+  link.addEventListener('click', function () {
+    nav.classList.remove('show')
+  })
 }
-*/
+
+/* ADICIONAR SHADOW NO HEADER AO DAR SCROLL */
+
+const header = document.querySelector('#header')
+const navHeight = header.offsetHeight
+
+function changeHeaderWhenScroll() {
+  if (window.scrollY >= navHeight) {
+    header.classList.add('scroll')
+  } else {
+    header.classList.remove('scroll')
+  }
+}
+
+/* =================TESTIMONIALS SWIPER================= */
+
+const swiper = new Swiper('.swiper', {
+  slidesPerView: 1,
+  pagination: {
+    el: '.swiper-pagination'
+  },
+  mousewheel: true,
+  keyboard: true,
+  breakpoints: {
+    767: {
+      slidesPerView: 2,
+      setWrapperSize: true
+    }
+  }
+})
+
+/* =================SCROLL REVEAL================= */
+
+const scrollReveal = ScrollReveal({
+  origin: 'top',
+  distance: '32px',
+  duration: 700,
+  reset: true
+})
+
+scrollReveal.reveal(
+  `
+#about .image, #about .text,
+#episodes header, #episodes .cards, #episodes .buttons,
+#testimonials header, #testimonials .testimonials,
+#store .header, #store .cards, #store .buttons,
+#support .header, #support .cards, #support .subtitle,
+footer .logo, footer .social
+`,
+  { interval: 100 }
+)
+
+/* =====BACK TO THE TOP====== */
+
+const backToTopButton = document.querySelector('.back-to-top')
+
+function backToTop() {
+  if (window.scrollY >= 560) {
+    backToTopButton.classList.add('show')
+  } else {
+    backToTopButton.classList.remove('show')
+  }
+}
+
+/* MENU ATIVO CONFORME SEÇÃO VISÍVEL */
+
+const sections = document.querySelectorAll('main section[id]')
+
+function activateMenuAtCurrentSection() {
+  const checkpoint = window.pageYOffset + (window.innerHeight / 8) * 4
+
+  for (const section of sections) {
+    const sectionTop = section.offsetTop
+    const sectionHeight = section.offsetHeight
+    const sectionId = section.getAttribute('id')
+
+    const checkpointStart = checkpoint >= sectionTop
+    const checkpointEnd = checkpoint <= sectionTop + sectionHeight
+
+    if (checkpointStart && checkpointEnd) {
+      document
+        .querySelector('nav ul li a[href*=' + sectionId + ']')
+        .classList.add('active')
+    } else {
+      document
+        .querySelector('nav ul li a[href*=' + sectionId + ']')
+        .classList.remove('active')
+    }
+  }
+}
+
+/* QUANDO ROLA A PÁGINA */
+
+window.addEventListener('scroll', function () {
+  changeHeaderWhenScroll()
+  backToTop()
+  activateMenuAtCurrentSection()
+})
